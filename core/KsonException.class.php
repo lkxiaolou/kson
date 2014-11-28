9c4b499009ed84ae898aa0b062cf019d37585b02
lkxiaolou <lkxiaolou@163.com>
2014-11-28 14:19:46 +0800

    add my exception class

---
+++ core/KsonException.class.php
@@ -0,0 +1,37 @@
+<?php
+    if(!defined('IS_KSON')) die('Access Denied!');
+    class KsonException extends Exception{
+        
+        public function __construct($message, $code){
+            $this->message = $message;
+            $this->code = $code;
+        }
+        
+        //输出错误信息
+        public function error()
+        {
+            //调试模式不输出错误
+            $is_debug = Fun::getConfig('deBug');
+            if(!$is_debug){
+                $this->message = '系统繁忙';
+            }
+            //是否为ajax请求
+            if($this->isAjax()){
+                echo json_encode(array('msg' => $this->message, 'code' => $this->code));
+            }
+            else{
+                echo $this->message;
+            }
+            die();
+        }
+        
+        //判断是否为ajax请求
+        private function isAjax()
+        {
+            if ($_SERVER['HTTP_X_REQUESTED_WITH'] && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') return true;
+            if ($_POST['is_ajax'] || $_GET['is_ajax']) return true;
+            return false;
+        }
+    
+    }
+?>
