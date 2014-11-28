10e3adfa1104f0e028b884fc1d9df9ad7ee9da1f
lkxiaolou <lkxiaolou@163.com>
2014-11-28 14:41:57 +0800

    format code

--- core/kson.lang.php
+++ core/kson.lang.php
@@ -5,5 +5,5 @@
     $kson_lang['file not exists']    = '文件不存在';
     $kson_lang['action not exists']  = '方法不存在';
     $kson_lang['clone is not allow'] = '不允许复制';
-    $kson_lang['mysql query error'] = '数据库查询出错';
+    $kson_lang['mysql query error']  = '数据库查询出错';
 ?>
