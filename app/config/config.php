8f63e127b52cb3362973a1d13124f3ddc2efbb4f
lkxiaolou <lkxiaolou@163.com>
2014-11-28 16:09:40 +0800

    add app view config and format the code

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,9 @@
 <?php
 	//mysql 配置
-	$app_config['MysqlHost'] = '127.0.0.1';
+	$app_config['MysqlHost']   = '127.0.0.1';
 	$app_config['MysqlDbName'] = 'oa';
-	$app_config['MysqlUser'] = 'root';
-	$app_config['MysqlPwd'] = 'lkxiaolou';
+	$app_config['MysqlUser']   = 'root';
+	$app_config['MysqlPwd']    = 'lkxiaolou';
+	$app_config['viewExt']     = '.html';
+	$app_config['viewDir']     = APP_PATH . '/v';
 ?>
