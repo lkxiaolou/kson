e083b9e2988017e2eaf09144fbfccf5c1619fa57
lkxiaolou <lkxiaolou@163.com>
2014-11-28 16:08:57 +0800

    format the code

--- core/kson.lang.php
+++ core/kson.lang.php
@@ -1,9 +1,10 @@
 <?php
     if(!defined('IS_KSON')) die('Access Denied!');
     //系统语言
-    $kson_lang['class not exists']   = '类不存在';
-    $kson_lang['file not exists']    = '文件不存在';
-    $kson_lang['action not exists']  = '方法不存在';
-    $kson_lang['clone is not allow'] = '不允许复制';
-    $kson_lang['mysql query error']  = '数据库查询出错';
+    $kson_lang['class not exists']     = '类不存在';
+    $kson_lang['file not exists']      = '文件不存在';
+    $kson_lang['action not exists']    = '方法不存在';
+    $kson_lang['clone is not allow']   = '不允许复制';
+    $kson_lang['mysql query error']    = '数据库查询出错';
+    $kson_lang['view file not exists'] = '模版文件不存在';
 ?>
