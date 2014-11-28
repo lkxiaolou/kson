1a82df75d05f46cff8a6139c65ed8b4fd1f774fd
lkxiaolou <lkxiaolou@163.com>
2014-11-28 14:20:18 +0800

    just a test

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,5 @@
 	$app_config['MysqlHost'] = '127.0.0.1';
 	$app_config['MysqlDbName'] = 'oa';
 	$app_config['MysqlUser'] = 'root';
-	$app_config['MysqlPwd'] = '';
+	$app_config['MysqlPwd'] = 'lkxiaolou';
 ?>
