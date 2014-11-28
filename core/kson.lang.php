d7a1eaeb85b975132ce0806ac3bd024866a58ed5
lkxiaolou <lkxiaolou@163.com>
2014-11-28 14:19:20 +0800

    add error message

--- core/kson.lang.php
+++ core/kson.lang.php
@@ -1,8 +1,9 @@
 <?php
+    if(!defined('IS_KSON')) die('Access Denied!');
     //系统语言
-    
     $kson_lang['class not exists']   = '类不存在';
     $kson_lang['file not exists']    = '文件不存在';
     $kson_lang['action not exists']  = '方法不存在';
     $kson_lang['clone is not allow'] = '不允许复制';
+    $kson_lang['mysql query error'] = '数据库查询出错';
 ?>
